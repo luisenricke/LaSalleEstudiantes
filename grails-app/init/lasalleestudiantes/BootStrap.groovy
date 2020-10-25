579d377b801e2dbf89c6c9fc1c9f5b7ee9bff8a8
Luis Villalobos <luisevm24@outlook.com>
2020-10-25 01:32:51 -0600

    inicializando a NivelAcademico y Especializacion

--- grails-app/init/lasalleestudiantes/BootStrap.groovy
+++ grails-app/init/lasalleestudiantes/BootStrap.groovy
@@ -1,9 +1,28 @@
 package lasalleestudiantes
 
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        creandoNivelAcademico 'Bachillerato', []
+        creandoNivelAcademico "Licenciatura", ["Enfermería", "Software", "Arquitectura"]
+        creandoNivelAcademico "Maestría", ["Fiscal", "Educación"]
+        creandoNivelAcademico "Doctorado", ["Comunicación", "Gastronomía"]
     }
+
     def destroy = {
+    }
+
+    def static creandoNivelAcademico(String nombre, List<String> especializaciones) {
+        if (nombre.isBlank()) return
+
+        NivelAcademico _nivelAcademico = new NivelAcademico(nombre: nombre)
+        _nivelAcademico.save()
+
+        for (especializacion in especializaciones) {
+            Especializacion _especializacion = new Especializacion(nombre: especializacion, nivelAcademico: _nivelAcademico)
+            _especializacion.save()
+        }
     }
 }
