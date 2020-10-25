139ac161e452c2b97e16f3e3eebba8207fbf55e1
Luis Villalobos <luisevm24@outlook.com>
2020-10-25 03:05:37 -0600

    cambiando de idioma por defecto

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
-// Place your Spring DSL code here
+package spring
+
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+
 beans = {
+    localeResolver(SessionLocaleResolver) {
+        defaultLocale= new Locale('es')
+    }
 }
