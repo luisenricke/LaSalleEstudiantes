3e1015d9631934d335b11ef6a23aa4a2c6d38f3a
Luis Villalobos <luisevm24@outlook.com>
2020-10-25 01:32:13 -0600

    modificando en plural la variable

--- grails-app/domain/lasalleestudiantes/Especializacion.groovy
+++ grails-app/domain/lasalleestudiantes/Especializacion.groovy
@@ -5,7 +5,7 @@ class Especializacion {
     String nombre
 
     static belongsTo = [nivelAcademico: NivelAcademico]
-    static hasMany = [historialAcademico: HistorialAcademico]
+    static hasMany = [historialesAcademicos: HistorialAcademico]
 
     static constraints = {
         nombre minSize: 5, blank: false, unique: true
